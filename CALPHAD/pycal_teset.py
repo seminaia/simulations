1e541250f77f9da3f527597241038083e61c2589
Soki <155200190+seminaia@users.noreply.github.com>
2026-03-25 17:57:07 -0400

    New tdb file

--- CALPHAD/pycal_teset.py
+++ CALPHAD/pycal_teset.py
@@ -26,23 +26,3 @@ binplot(
     }
 )
 plt.show()
-Traceback (most recent call last):
-  File "/home/soki/simulations/CALPHAD/pycal_teset.py", line 5, in <module>
-    dbf = Database('cost507.tdb')
-  File "/home/soki/miniconda3/envs/myCALPHADenv/lib/python3.13/site-packages/pycalphad/io/database.py", line 115, in __new__
-    return cls.from_file(fname, fmt=fmt)
-           ~~~~~~~~~~~~~^^^^^^^^^^^^^^^^
-  File "/home/soki/miniconda3/envs/myCALPHADenv/lib/python3.13/site-packages/pycalphad/io/database.py", line 224, in from_file
-    format_registry[fmt.lower()].read(dbf, fd)
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^
-  File "/home/soki/miniconda3/envs/myCALPHADenv/lib/python3.13/site-packages/pycalphad/io/tdb.py", line 985, in read_tdb
-    raise e
-  File "/home/soki/miniconda3/envs/myCALPHADenv/lib/python3.13/site-packages/pycalphad/io/tdb.py", line 968, in read_tdb
-    tokens = grammar.parseString(command)
-  File "/home/soki/miniconda3/envs/myCALPHADenv/lib/python3.13/site-packages/pyparsing/util.py", line 466, in _inner
-    return fn(self, *args, **kwargs)
-  File "/home/soki/miniconda3/envs/myCALPHADenv/lib/python3.13/site-packages/pyparsing/core.py", line 1346, in parse_string
-    raise exc.with_traceback(None)
-pyparsing.exceptions.ParseException: Invalid TDB syntax.
- ELEMENT  Y    HCP_A3              8.89059+01  5.9664E+03  4.4434E+01 
-                                          ^, found '+'  (at char 2013), (line:138, col:42)

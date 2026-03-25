c3b1008c7140fdc295e378a7bd17b98616790fca
Soki <155200190+seminaia@users.noreply.github.com>
2026-03-25 17:04:19 -0400

    Refactor Pycalphad code

--- CALPHAD/pycal_teset.py
+++ CALPHAD/pycal_teset.py
@@ -1,18 +1,27 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from pycalphad import Database, equilibrium, binplot
+from pycalphad import Database, binplot
 import pycalphad.variables as v
 
 dbf = Database('PrecHiMn-04.tdb')
 
-my_phases = ['LIQUID', 'FCC_A1', 'BCC_A2', 'GAS','HCP_A3','FE4N_L1']
-my_components = ['FE','CR', 'N', 'C']
-dof = len(my_components)-len(my_phases) + 2
-print(f"Degrees of freedom: {dof}")
-fig = plt.figure()
-axes = fig.gca()
+components = ['FE', 'CR', 'C', 'N', 'VA']
+phases = ['LIQUID', 'FCC_A1', 'BCC_A2', 'HCP_A3', 'FE4N_L1']
 
-binplot(dbf, my_components, my_phases, {v.X('C'):(0,1,0.01), v.N:1, 
-                                       v.T:(300,2000,10), v.P:101325})
+fig, ax = plt.subplots()
+
+binplot(
+    dbf,
+    components,
+    phases,
+    {
+        v.X('CR'): (0, 0.5, 0.01),
+        v.X('C'): 0.01,
+        v.X('N'): 0.01,
+        v.T: (300, 2000, 10),
+        v.P: 101325,
+        v.N: 1
+    },
+    ax=ax
+)
 
 plt.show()

2bc75309cc04561de3070e68900cf2c17f125214
Soki <155200190+seminaia@users.noreply.github.com>
2026-03-30 13:49:32 -0400

    Fix doc_builder to handle latex math in rows

--- PyFunc/doc_builder.py
+++ PyFunc/doc_builder.py
@@ -847,44 +847,38 @@ class DocumentBuilder:
             with container.create(Enumerate()) as enum:
                 for item in block.items:
                     enum.add_item(item)
-        elif block.kind == "table":
-            colspec = block.alignment or ("c" * len(block.headers))
-            if block.longtable:
-                lt = LongTable(colspec)
-                lt.add_hline()
-                lt.add_row(block.headers)
-                lt.add_hline()
-                lt.end_table_header()
-                for row in block.rows:
-                    lt.add_row([
-                        _fmt_cell(v, block.float_fmt) if isinstance(v, float) else str(v)
-                        for v in row
-                    ])
-                lt.add_hline()
-                container.append(lt)
-                if block.caption:
-                    container.append(NoEscape(rf"\captionof{{table}}{{{escape_latex(block.caption)}}}"))
-                if block.label:
-                    container.append(NoEscape(rf"\label{{{block.label}}}"))
-            else:
-                container.append(NoEscape(r"\begin{table}[H]"))
-                container.append(NoEscape(r"\centering"))
-                tab = Tabular(colspec)
-                tab.add_hline()
-                tab.add_row(block.headers)
-                tab.add_hline()
-                for row in block.rows:
-                    tab.add_row([
-                        _fmt_cell(v, block.float_fmt) if isinstance(v, float) else str(v)
-                        for v in row
-                    ])
-                tab.add_hline()
-                container.append(tab)
-                if block.caption:
-                    container.append(NoEscape(rf"\caption{{{escape_latex(block.caption)}}}"))
-                if block.label:
-                    container.append(NoEscape(rf"\label{{{block.label}}}"))
-                container.append(NoEscape(r"\end{table}"))
+elif block.kind == "table":
+    colspec = block.alignment or ("c" * len(block.headers))
+    if block.longtable:
+        lt = LongTable(colspec, escape=False)
+        lt.add_hline()
+        lt.add_row([_render_table_cell(h, block.float_fmt) for h in block.headers])
+        lt.add_hline()
+        lt.end_table_header()
+        for row in block.rows:
+            lt.add_row([_render_table_cell(v, block.float_fmt) for v in row])
+        lt.add_hline()
+        container.append(lt)
+        if block.caption:
+            container.append(NoEscape(rf"\captionof{{table}}{{{escape_latex(block.caption)}}}"))
+        if block.label:
+            container.append(NoEscape(rf"\label{{{block.label}}}"))
+    else:
+        container.append(NoEscape(r"\begin{table}[H]"))
+        container.append(NoEscape(r"\centering"))
+        tab = Tabular(colspec, escape=False)
+        tab.add_hline()
+        tab.add_row([_render_table_cell(h, block.float_fmt) for h in block.headers])
+        tab.add_hline()
+        for row in block.rows:
+            tab.add_row([_render_table_cell(v, block.float_fmt) for v in row])
+        tab.add_hline()
+        container.append(tab)
+        if block.caption:
+            container.append(NoEscape(rf"\caption{{{escape_latex(block.caption)}}}"))
+        if block.label:
+            container.append(NoEscape(rf"\label{{{block.label}}}"))
+        container.append(NoEscape(r"\end{table}"))
         elif block.kind == "figure":
             with container.create(Figure(position=block.position)) as fig:
                 fig.add_image(block.path, width=NoEscape(block.width))

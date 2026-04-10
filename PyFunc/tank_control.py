1a1fa2859b877575c78d8a34ba1a95b56584a62e
Soki Sem <seminaia401@gmail.com>
2026-04-10 10:22:31 -0400

    Refactor tank control simulation: streamline ODE handling and update plotting logic

--- PyFunc/tank_control.py
+++ PyFunc/tank_control.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import scipy.integrate
 import sympy as sp
 from sympy import Function, dsolve, diff, checkodesol
-from sympy.integrals.transforms import laplace_transform, inverse_laplace_transform, 
 from sympy.physics.mechanics import linearize
 from sympy.abc import F, t, s
 from IPython.display import display, Math
@@ -21,31 +20,28 @@ A, cv = sp.symbols(names='A cv', positive=True)
 hbar = sp.symbols('hbar', positive=True)  # Steady-state
 q_out = cv * sp.sqrt(h)
 ode = sp.Eq(diff(h, t), (q_in - q_out) / A)
-ode_lin = linearize(ode, [h, q_in], [hbar, 0])
-ode_lhs = ode.lhs
-ode_rhs = ode.rhs
 sp.pprint(ode)
+ode_rhs = sp.simplify(ode.rhs)
+sp.pprint(ode_rhs)
 f = sp.lambdify((h, q_in, A, cv),ode_rhs, modules='numpy')
-cv = 0.05 
-A = 0.5
+A0= 2
+cv = 0.05
 h0 = 1
-a =- cv/(2*A*np.sqrt(h0))
-b= 1/A
-c=1
-d=0
-lt_rhs = sp.laplace_transform(ode_rhs,t, s, noconds=True, simplify=True)
-lt_lhs = sp.laplace_transform(ode_lhs, t, s, noconds=True, simplify=True)
-lt = sp.Eq(lt_lhs, lt_rhs)
-laplace_correspondence(lt, {h: H, q_in: Q_in})
-sp.pprint(lt)
-inv_lt = sp.inverse_laplace_transform(lt, s, t, )
-sp.pprint(inv_lt)
-sys = ct.ss(a, b, c, d, inputs='q_in', outputs='h')
-plt.plot(solve, solve.y[0], 'b', label='Height h(t)')
+V0 = A0 * h0
+qin = 1 
+tspan = np.linspace(start=0, 1000, 1000)
+dt = tspan[1] - tspan[0]
+
+hspan =[]
+for t in tspan:
+    hdot = f(h0, qin, A0, cv)
+    h0 += hdot * dt
+    hspan.append(h0)
+plt.plot(tspan, hspan)
 plt.xlabel('Time [s]')
 plt.ylabel('Height h [m]')
-plt.title(label='Tank Level Response to Initial Condition')
-plt.grid(True)
+plt.title('Tank Level Response to Initial Condition')
+#sys = ct.ss(a, b, c, d, inputs='q_in', outputs='h')
 plt.show()
 # Step response (input magnitude = 1 by default)
 response = ct.step_response(sysdata=sys, X0=[1])

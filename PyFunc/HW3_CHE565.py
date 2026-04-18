2ced3834c37e418a4c610632965ef5b185198fe5
Soki Sem <seminaia401@gmail.com>
2026-04-18 22:13:59 +0000

    Apr 18, 2026, 6:13 PM

--- PyFunc/HW3_CHE565.py
+++ PyFunc/HW3_CHE565.py
@@ -113,8 +113,11 @@ headings_B = ["Grade","Specifications","Selling Price ($/bbl)"]
 t(headings_B, rows_B, float_fmt=".2f", alignment="c p{4cm} c")
 c1, c2, c3, c4 = sp.symbols('c1 c2 c3 c4')
 A, B, C = sp.symbols('A B C')
-grade_A = 0.15*c1 + 0.40*c2 
-
+constrains = [
+    (A < 0.15*c1 + 0.40*c2)&
+    (B < 0.50*c3 + 0.10*c1)&
+    (C < 0.10*c2 + 0.20*c1)
+]
 txt_file, tex_file, pdf_file = doc.save_all()
 
 print(f"Wrote text log: {txt_file}")

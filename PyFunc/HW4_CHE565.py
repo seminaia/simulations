772a8f657f3880ea33f51a8cdd646483abe8d98b
Soki Sem <seminaia401@gmail.com>
2026-04-23 13:50:29 -0400

    Refactor code structure for improved readability and maintainability

---
+++ PyFunc/HW4_CHE565.py
@@ -0,0 +1,84 @@
+from math import e
+from os import name
+from re import A
+import re
+from urllib import response
+from IPython.core import display_functions
+import control as ct
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sp
+from sympy.integrals import inverse_laplace_transform
+from sympy.abc import B, G, x,t,s
+from scipy.constants import g
+from tbcontrol import blocksim
+from scipy import signal
+sp.init_printing()
+# xdot = Ax + Bu in deviation, where x = h - hbar, u = qin - qbar
+# y = Cx + Du  # output is just the height deviation, so C = 1, D = 0
+tau_i, tau_D, tau_p = sp.symbols('tau_i tau_D tau_p')
+K_c,K_p = sp.symbols('K_c K_p')
+s = ct.tf('s')
+taui = 1
+taup = 5
+tauD = 2
+Kc = 1
+Kp = 1
+I = 1/taui
+Gp = Kp/(taup*s + 1)
+Gp_block = blocksim.LTI('Gp(s)','P','Y',[Kp],[taup, 1],delay=tauD)
+Gc = Kc*(1+I/(s))
+Gc_block = blocksim.PI('Gc(s)','E','Yc',Kc,taui)
+blocks = [Gp_block, Gc_block]
+sums = {'E':['+Ysp','-Y'],
+        'P':['+Yc', '+d']}
+inputs = {'Ysp':blocksim.step(),
+          'd':blocksim.step()}
+
+Us=1/s
+Ut = sp.Heaviside(t)
+Ut_func = sp.lambdify(t, Ut, 'numpy')
+Gd = 1/s
+Gt = sp.Heaviside(t)
+Gt_func = sp.lambdify(t, Gt, 'numpy')
+GD_numer, GD_denom = ct.delay.pade(tauD,3)
+
+G_c_ct = ct.tf(Gc, name='Gc(s)', inputs='E', outputs='Yc')
+G_p_ct = ct.tf(Gp, name='Gp(s)', inputs='P', outputs='Yp')
+G_d_ct = ct.tf(Gd, name='Gd(s)', inputs='D', outputs='Yd')
+G_D_ct = ct.tf(GD_numer, GD_denom, name='GD(s)', inputs='U', outputs='Y')
+sum1 = ct.summing_junction(['Ysp','-Y'], ['E'], name='Sum1')
+sum2 = ct.summing_junction(['Yc', 'Yd'], ['P'], name='Sum2')
+system = ct.interconnect([G_c_ct, G_p_ct, G_d_ct, G_D_ct, sum1, sum2], inplist=['Ysp', 'Yd'], outlist=['Y'], )
+print("Closed-loop transfer function G(s):")
+print(system)
+print("Controller transfer function Gc(s):")
+print(G_c_ct)
+print("Plant transfer function Gp(s):")
+print(G_p_ct)
+print("Disturbance transfer function Gd(s):")
+print(G_d_ct)
+print("Delay Pade approximation")
+print(GD_numer, GD_denom)
+print("Delay G_D(s):")
+print(G_D_ct)
+
+t_vals = np.arange(0, 50, 0.1)
+diagram = blocksim.Diagram(blocks=blocks, sums=sums, inputs=inputs)
+simulation = diagram.simulate(t_vals,True)
+results = simulation['Y']
+Ut_vals = Ut_func(t_vals)
+Gt_vals = Gt_func(t_vals)
+print(f"Inputs : {Ut}, {Gt}")
+response = ct.forced_response(sysdata=system, T=t_vals, U=[Ut_vals, Gt_vals])
+
+#for signal, values in simulation.items():
+    #plt.plot(t_vals, values, label=f'Block Diagram Simulation: {signal}', linestyle='-')
+plt.plot(response.time, response.outputs[0], label='Step Response from Control Library', linestyle='--')
+plt.plot(t_vals, response.inputs[0], label='Input Step Function', linestyle=':')
+plt.legend()
+plt.xlabel('Time (s)')
+plt.ylabel('g(t)')
+plt.title('Block Diagram Response')
+plt.grid()
+plt.show()

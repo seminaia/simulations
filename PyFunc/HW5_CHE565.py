e8a6cfb960e4d02b9cebb593e225192af38d1143
Soki Sem <seminaia401@gmail.com>
2026-04-29 15:33:27 -0400

    Update HW5_CHE565.py and related files for improved functionality and documentation clarity

--- PyFunc/HW5_CHE565.py
+++ PyFunc/HW5_CHE565.py
@@ -7,8 +7,6 @@ Results are written to HW5_CHE565.txt, HW5_CHE565.tex, and HW5_CHE565.pdf
 """
 
 from math import tau
-from re import S
-
 
 import numpy as np
 import matplotlib
@@ -19,7 +17,7 @@ import sympy as sp
 import matplotlib.pyplot as plt
 from doc_builder import DocumentBuilder
 import control as ct
-
+import control.optimal as ct_opt
 OUTPUT_FILE = "HW5_CHE565"
 PLOT_FILE = "HW5_CHE565_plot.png"
 report_lines = []
@@ -78,7 +76,7 @@ y_eq = sp.Eq(y, C*x + D*u)
 sp.pprint(xdot_eq)
 sp.pprint(y_eq)
 # Time vector
-tvals = np.linspace(0, 50, 1000)
+tvals = np.linspace(0, 50, 100)
 step_on = np.ones_like(tvals)
 step_off = np.zeros_like(tvals)
 
@@ -96,8 +94,8 @@ def build_closed_loop(Kc1, Kc2, tauI1, tauI2, cascade=False):
     Gp1 = Kp1 / (taup1 * s + 1)
     Gp2 = Kp2 / (taup2 * s + 1)
     Gd = ct.tf([1], [1])    # direct disturbance addition
-    GD= ct.tf(numD, denD, name='GD', inputs='Yp2', outputs='Y')
-    
+    GD1= ct.tf(numD, denD, name='GD1', inputs='Yp1', outputs='YD')
+    GD2= ct.tf(numD, denD, name='GD2', inputs='Yp2', outputs='Y')
     # State Space Representation of the blocks for interconnection
     # Note: the control library's interconnect function works better with state-space models, so we convert the transfer functions to state-space form.
     # xdot = Ax + Bu
@@ -106,32 +104,40 @@ def build_closed_loop(Kc1, Kc2, tauI1, tauI2, cascade=False):
     Gc1_blk = ct.ss(Gc1, name='Gc1', inputs='E1', outputs='Yc1')
     Gc2_blk = ct.ss(Gc2, name='Gc2', inputs='E2', outputs='Yc2')
     Gp1_blk = ct.ss(Gp1, name='Gp1', inputs='Yc2', outputs='Yp1')
-    Gp2_blk = ct.ss(Gp2, name='Gp2', inputs='P', outputs='Yp2')
+    Gp2_blk = ct.ss(Gp2, name='Gp2', inputs='P', outputs='Y')
     Gd_blk = ct.ss(Gd, name='Gd', inputs='D', outputs='Yd')     # direct disturbance addition
-    GD_blk = ct.ss(GD, name='GD', inputs='Yp2', outputs='Y')
+    #GD1_blk = ct.ss(GD1, name='GD1', inputs='Yp1', outputs='YD')
+    #GD2_blk = ct.ss(GD2, name='GD2', inputs='Yp2', outputs='Y')
     sum1 = ct.summing_junction(inputs=['Ysp', '-Y'], output='E1', name='Sum1')
     if cascade:
          sum2 = ct.summing_junction(inputs=['Yc1', '-P'], output='E2', name='Sum2')
     else:
         sum2 = ct.summing_junction(inputs=['Yc1'], output='E2', name='Sum2')
     sum3 = ct.summing_junction(inputs=['Yp1', 'Yd'], output='P', name='Sum3')
-    blocks = [Gc1_blk, Gc2_blk, Gp1_blk, Gp2_blk, Gd_blk, GD_blk]
-    #for blk in blocks:
-        #print(blk.name, "states = ", blk.nstates)
+    control_blks = [Gc1_blk, Gc2_blk]
+    plant_blks = [Gp1_blk, Gp2_blk]
+    disturbance_blks = [Gd_blk]
+    #delay_blks = [GD1_blk, GD2_blk]
+    sum_blks = [sum1, sum2, sum3]
+    blocks = control_blks + plant_blks + disturbance_blks + sum_blks
     sys = ct.interconnect(
-        blocks + [sum1, sum2, sum3],
+        blocks,
         input=['Ysp', 'D'],
         output=['Y'],
         input_prefix = ["Ysp", "D"],
         output_prefix = ["Y"],
     )
-    
     return sys
 
+    
 def calculate_IAE(t, y, ysp):
     error = ysp - y
     iae = np.trapezoid(np.abs(error), t)
     return iae
+def simulate_case(sys, t, ysp_input, d_input):
+    U = np.vstack([ysp_input, d_input])
+    resp = ct.forced_response(sys, T=t, U=U, squeeze=True,return_states=True)
+    return resp.time, resp.outputs, resp.inputs, resp.states
 
 def tuning_lqr(sys, Q, R):
     # Convert to state-space if not already
@@ -145,16 +151,6 @@ def tuning_lqr(sys, Q, R):
     K, S, E = ct.lqr(A, B, Q, R,)
     
     return K, S, E
-def simulate_case(sys, t, ysp_input, d_input):
-    U = np.vstack([ysp_input, d_input])
-    resp = ct.forced_response(sys, T=t, U=U, squeeze=True,return_states=True)
-    return resp.time, resp.outputs, resp.inputs, resp.states
-
-
-# ---------------------------
-# Simulation helper
-# ---------------------------
-
 
 
 def save_plot(filename, t, y, title, ysp=None, d=None):
@@ -180,65 +176,21 @@ def save_plot(filename, t, y, title, ysp=None, d=None):
 # =============================================================================
 
 sys1 = build_closed_loop(Kc1, Kc2, tauI1, tauI2)
-A1, B1, C1, D1 = sys1.A, sys1.B, sys1.C, sys1.D
 print(sys1)
-t1, y1, u1, x1 = simulate_case(sys1, tvals, step_off, step_on)
+A1, B1, C1, D1 = sys1.A, sys1.B, sys1.C, sys1.D
+n = A1.shape[0]
 
+t1, y1, u1, x1 = simulate_case(sys1, tvals, step_off, step_on)
 iae1 = calculate_IAE(t1, y1, step_off)
-def objective_PI(params):
-    P, I = params
-    if P <= 0 or I <= 0:
-        return np.inf  # Penalize non-positive parameters
-    tauI = 1 / I
-    sys = build_closed_loop(P, Kc2, tauI, tauI2)
-    if not np.all(np.real(ct.poles(sys)) < 0):
-        return np.inf
-    
-    t, y, u, x = simulate_case(sys, tvals, step_off, step_on)
-    y = np.asarray(y).squeeze()
-    t = np.asarray(t).squeeze()
-    u = np.asarray(u).squeeze()
-    x = np.asarray(x).squeeze()
-    ysp = np.asarray(step_off).squeeze()
-    n=x.shape[0]
-    m=u.shape[1]
-    print(f"Shapes:",{n, m})
-    e = ysp - y
-    eint = np.trapezoid(np.abs(e), t)
-    u_control = P*(e+I*eint)
-    Q = np.eye(m)
-    R = np.eye(m)
-    print("Q, R shapes:", Q.shape, R.shape)
-    integrand = np.zeros_like(t)
-    print(len(t))
-    for k in range(len(t)):
-        xk = x[k, :]
-        print(f"xk shape: {xk.shape}")
-        uk = u[k, :]
-        print(f"uk shape: {uk.shape}")
-        integrand[k] = xk.T @ Q @ xk + uk.T @ R @ uk
-    J = np.trapezoid(integrand, t)
-    iae = np.trapezoid(np.abs(e), t)
-    return J
-initial_guess = [Kc1_nom, I1_nom]
-result = minimize(objective_PI, initial_guess)
-print("Optimal P, I:", result.x)
+constraints =[ct_opt.input_range_constraint(sys1, [0,0],[0,1])]
+Gc_x1 = x1[0, :]
+Gp1_x1 = x1[1, :]
+Gp2_x1 = x1[2, :]
 A1_sp = sp.Matrix(A1)
 B1_sp = sp.Matrix(B1)
 C1_sp = sp.Matrix(C1)
 D1_sp = sp.Matrix(D1)
-n = A1.shape[0]
-m = B1.shape[1]
-Q = np.eye(n)
-R = np.eye(m)
-[K1, S1, E1] = tuning_lqr(sys1, Q, R)
-K1_sp = sp.Matrix(K1)
-S1_sp = sp.Matrix(S1)
-E1_sp = sp.Matrix(E1)
-save_plot("closed_loop_no_cascade.png",
-          t1,
-          y1,
-          "Closed-loop response to unit step in disturbance", ysp=step_off, d=step_on)
+
 # =============================================================================
 # Simulate Response to Step Disturbance with No Cascade Control and No Setpoint Change using Simulink autotuned values
 # =============================================================================
@@ -290,7 +242,6 @@ images = [
     [("closed_loop_no_cascade_setpoint_simulink.png", "Simulink"), ("closed_loop_no_cascade_setpoint.png", "Python")],
     [("closed_loop_no_cascade_setpoint_simulink_autotuning.png", "Simulink"), ("closed_loop_no_cascade_setpoint_autotuning.png", "Python")],
 ]
-num_approx, den_approx = ct.delay.pade(theta, pade_order)
 # =============================================================================
 # Document writeup
 # =============================================================================
@@ -298,27 +249,22 @@ doc.section("Introduction")
 px(
 f" The following homework was done with Simulink and the Control Systems Library in Python. The block diagram of the system is shown in ", doc.figref("fig:block_diagram"),
 ". ",
-"The block diagram was created in simulink and then I built the same diagram as a python function using the control library. ")
+"The block diagram was created in simulink and then I built the same diagram as a python function using the control library. Simulink was mainly used as a sanity check for the response of the system in the python code. ")
 p(
-"Note: sum2 in the block diagram is the summing junction that takes the first controller output and subtract the P stream in order to form the inner loop. But, first in order to simulate without the inner loop, so I set cascade=False. This just converts the controller output (Yc1) to the error signal (E2) for the second controller. "
-"The disturbance D is added directly to the output of Gp1 (Yp1) " 
+"Note: sum2 in the block diagram is the summing junction that takes the first controller output and subtract the P stream in order to form the inner loop. But, first in order to simulate without the inner loop, so I set cascade=False. This just converts the controller output (Yc1) to the error signal (E2) for the second controller. The disturbance D is added directly to the output of Gp1 (Yp1) " 
 )
 px(
 "The transport delay transfer function is approximated using a Pade approximation of order ", 
-pade_order,im(r'\ '),
-" and a delay of ",
-im(r"\theta_d = "), 
-theta)
+pade_order,"." ," Although it should be relatively straightforward to simulate with delay in simulink and in python, I commented through the delay blocks in simulink and in python. The assignment didn't specifiy the delay time. Also, the control library has the delay function, but it only uses the Pade approximation.",)
 line(
-" which gives the following transfer function approximation: ")
-num_poly = sp.Poly(num_approx, sp.symbols('s'))
-den_poly = sp.Poly(den_approx, sp.symbols('s'))
-
+" Which gives the following transfer function approximation: ")
+num_approx_sym = 1 - (sp.symbols('theta_d'))/2 * sp.symbols('s')
+den_approx_sym = 1 + (sp.symbols('theta_d'))/2 * sp.symbols('s')
 eq(
 sp.latex(
         sp.Eq(
             sp.exp(-sp.Symbol("theta_d") *sp.symbols('s')),
-            num_poly / den_poly
+            num_approx_sym / den_approx_sym
             )
          )
 )
@@ -339,46 +285,50 @@ def build_closed_loop(Kc1, Kc2, tauI1, tauI2, cascade=False):
     Gp1 = Kp1 / (taup1 * s + 1)
     Gp2 = Kp2 / (taup2 * s + 1)
     Gd = ct.tf([1], [1])    # direct disturbance addition
-    GD= ct.tf(numD, denD, name='GD', inputs='Yp2', outputs='Y')
+    GD1= ct.tf(numD, denD, name='GD1', inputs='Yp1', outputs='YD')
+    GD2= ct.tf(numD, denD, name='GD2', inputs='Yp2', outputs='Y')
     # State Space Representation of the blocks for interconnection
     # Note: the control library's interconnect function works better with state-space models, so we convert the transfer functions to state-space form.
     # xdot = Ax + Bu
     # y = Cx + Du
-    #
+    
     Gc1_blk = ct.ss(Gc1, name='Gc1', inputs='E1', outputs='Yc1')
     Gc2_blk = ct.ss(Gc2, name='Gc2', inputs='E2', outputs='Yc2')
     Gp1_blk = ct.ss(Gp1, name='Gp1', inputs='Yc2', outputs='Yp1')
-    Gp2_blk = ct.ss(Gp2, name='Gp2', inputs='P', outputs='Yp2')
+    Gp2_blk = ct.ss(Gp2, name='Gp2', inputs='P', outputs='Y')
     Gd_blk = ct.ss(Gd, name='Gd', inputs='D', outputs='Yd')     # direct disturbance addition
-    GD_blk = ct.ss(GD, name='GD', inputs='Yp2', outputs='Y')
-    sum1 = ct.summing_junction(inputs=['Ysp', '-Yp2'], output='E1', name='Sum1')
+    #GD1_blk = ct.ss(GD1, name='GD1', inputs='Yp1', outputs='YD')
+    #GD2_blk = ct.ss(GD2, name='GD2', inputs='Yp2', outputs='Y')
+    sum1 = ct.summing_junction(inputs=['Ysp', '-Y'], output='E1', name='Sum1')
     if cascade:
          sum2 = ct.summing_junction(inputs=['Yc1', '-P'], output='E2', name='Sum2')
     else:
         sum2 = ct.summing_junction(inputs=['Yc1'], output='E2', name='Sum2')
     sum3 = ct.summing_junction(inputs=['Yp1', 'Yd'], output='P', name='Sum3')
+    control_blks = [Gc1_blk, Gc2_blk]
+    plant_blks = [Gp1_blk, Gp2_blk]
+    disturbance_blks = [Gd_blk]
+    #delay_blks = [GD1_blk, GD2_blk]
+    sum_blks = [sum1, sum2, sum3]
+    blocks = control_blks + plant_blks + disturbance_blks + sum_blks
     sys = ct.interconnect(
-        [Gc1_blk, Gc2_blk, Gp1_blk, Gp2_blk, Gd_blk, sum1, sum2, sum3],
+        blocks,
         input=['Ysp', 'D'],
-        output=['Yp2'],
+        output=['Y'],
         input_prefix = ["Ysp", "D"],
-        output_prefix = ["Yp2"],
+        output_prefix = ["Y"],
     )
     return sys
 
+    
 def calculate_IAE(t, y, ysp):
     error = ysp - y
     iae = np.trapezoid(np.abs(error), t)
     return iae
-
-# ---------------------------
-# Simulation helper
-# ---------------------------
 def simulate_case(sys, t, ysp_input, d_input):
     U = np.vstack([ysp_input, d_input])
-    resp = ct.forced_response(sys, T=t, U=U, squeeze=True)
-    return resp.time, resp.outputs
-
+    resp = ct.forced_response(sys, T=t, U=U, squeeze=True,return_states=True)
+    return resp.time, resp.outputs, resp.inputs, resp.states
 """])
 doc.section("Problem 1")
 doc.subsection("Closed-loop Response to Step Disturbance")
@@ -393,11 +343,6 @@ figlog(
 
 px("The Simulink block diagram is shown in ", doc.figref("fig:block_diagram"), ".")
 
-p(
-    f"Case 1: Step disturbance with no setpoint change gave and step in disturbance with no setpoint change gave "
-    f"IAE = {iae1:.4f} in Python and IAE = 725.8109 in Simulink."
-)
-
 subfiglog(
     images[0],
     caption="Closed-loop response to no step change in setpoint and a unit step change in disturbance with no cascade control and no autotuning.",
@@ -406,20 +351,33 @@ subfiglog(
 )
 
 px(
-    "The closed-loop disturbance response and no cascade loop is shown in ",
+    f"Case 1: Step disturbance with no setpoint change gave and step in disturbance with no setpoint change gave ",
+    f"IAE = {iae1:.4f} in Python and IAE = 725.8109 in Simulink.",
+     "The closed-loop disturbance response and no cascade loop is shown in ",
     doc.figref("fig:no_cascade_step_disturbance"),
     "."
 )
+
+#figlog(
+#    "closed_loop_no_cascade_optimized.png",
+#    caption="Closed-loop response to unit step disturbance with optimized PI parameters.",
+#    label="fig:no_cascade_step_disturbance_optimized",
+#    width=r"0.8\textwidth",
+#)
+# px(
+    # "The closed-loop disturbance response with optimized PI parameters is shown in ",
+    # doc.figref("fig:no_cascade_step_disturbance_optimized"),
+    # ".",
+    # f" The optimization reduced the IAE from {iae1:.4f} to  in Python. ",
+# )
+
 
 
 p(
     f"The PI controller was then tuned using the autotuning feature in Simulink, "
     f"which gave P = {P1:.4f} and I = {I1:.4f}."
 )
-p(
-    f"Case 2: Step disturbance with no setpoint change and PI autotuning gave "
-    f"IAE = {iae2:.4f} in Python and IAE = 13.0463 in Simulink."
-)
+
 
 subfiglog(
     images[1],
@@ -430,6 +388,8 @@ subfiglog(
 
 
 px(
+    f"Case 2: Step disturbance with no setpoint change and PI autotuning gave ",
+    f"IAE = {iae2:.4f} in Python and IAE = 13.0463 in Simulink.",
     "The response to a unit step change in disturbance with no cascade control and PI autotuning is shown in ",
     doc.figref("fig:no_cascade_autotuning_step_disturbance"),
     "."
@@ -446,15 +406,14 @@ subfiglog(
     label="fig:no_cascade_step_setpoint",
 )
 
-px(
-    "The response to the step change in setpoint with no step disturbance is shown in ",
+px( 
+    f"Case 3: Step setpoint change with no disturbance change gave ",
+    f"IAE = {iae3:.4f} in Python and IAE = 2450.8205 in Simulink.",
+    "The response is shown in ",
     doc.figref("fig:no_cascade_step_setpoint"),
     "."
 )
-p(
-    f"Case 3: Step setpoint change with no disturbance change gave "
-    f"IAE = {iae3:.4f} in Python and IAE = 2450.8205 in Simulink."
-)
+
 
 subfiglog(
     images[3],
@@ -465,14 +424,11 @@ subfiglog(
 
 
 px(
-    "The response to a unit step change in setpoint with no disturbance change using PI autotuning is shown in ",
+    f"Case 4: Step setpoint change with no disturbance change and PI autotuning gave IAE = {iae4:.4f} in Python and IAE = 8.1939 in Simulink. The response is shown in ",
     doc.figref("fig:no_cascade_autotuning_step_setpoint"),
-    "."
+    ". ",
 )
-p(
-    f"Case 4: Step setpoint change with no disturbance change and PI autotuning gave "
-    f"IAE = {iae4:.4f} in Python and IAE = 8.1939 in Simulink."
-)
+   
 
 txt_file, tex_file, pdf_file = doc.save_all(runs=2)
 print(f"Wrote text log: {txt_file}")

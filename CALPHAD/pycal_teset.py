318c120c2f235a7b5d9cab84156812eb042cba94
Soki Sem <seminaia401@gmail.com>
2026-05-27 13:27:24 -0400

    Refactor settings and update pycal_teset.py for consistency in components and file handling

--- CALPHAD/pycal_teset.py
+++ CALPHAD/pycal_teset.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 from pycalphad import Database, binplot, equilibrium
 import pycalphad.variables as v
-f='COST507-modified.tdb'
-
+f="mmc1.TDB"
 dbf = Database(f)
 elements = dbf.elements
 elements.discard('/-')
-components = ['AL', 'MG','VA']
+components = ['FE', 'MN','Sn','C' ,'VA']
 phases = list(dbf.phases.keys())
 print("Elements:", elements)
 print("Number of elements:", len(elements))
@@ -19,19 +18,10 @@ binplot(
     components,
     phases,
     conditions={
-        v.X('MG'):(0,1,0.01),
-        #v.W('Mo'):0.157,
-        #v.W('Cr'):0.0711,
-        #v.W('Fe'):0.0371,
-        #v.W('Mn'):0.0056,
-        #v.W('Si'):0.0021,
-        #v.W('C') : 0.00058,
-        #v.W('W'): 0.0003,
-        #v.W('Cu'): 0.00014,
-        #v.W('B'): 0.000045,
-        #v.W('Al'): 0.0011,
-        #v.W('Co'): 0.0001,
-        #v.W('H'):(0, 1, 0.01),       
+        v.X('FE'):(0,1,0.1),
+        v.X('MN'):(0,1,0.1),
+        v.X('Sn'):(0,1,0.1),
+        v.X('C'):(0,1,0.1),
         v.T: (300, 1000, 10),
         v.P: 101325,
         v.N: 1

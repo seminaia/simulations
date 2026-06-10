d92b414fa9c84204a9d3238e259760bdd8acdb53
Soki Sem <seminaia401@gmail.com>
2026-06-10 03:01:33 +0000

    Jun 9, 2026, 11:01 PM

--- CALPHAD/pycal_teset.py
+++ CALPHAD/pycal_teset.py
@@ -14,10 +14,10 @@ tern_comp = ['FE', 'C', 'VA']
 phase_keys = list(dbf.phases.keys())  # Get all phase keys from the database
 filtered_phases_tern = filter_phases(dbf, tern_comp, phase_keys)  # Filter phases based on components
 conds_tern = {
-    v.T: (500, 1500, 100),  # vary from 500 to 1500 step 100
+    v.T: (500, 1600,100),  # vary from 500 to 1500 step 100
     v.P: 101325,
     v.N: 1,
-    v.X('C'): (0,0.08,0.01),  # vary from 0 to 0.08 step 0.01
+    v.X('C'): 0.025,  # vary from 0 to 0.08 step 0.01
 }    
 
 print(f"Phases considered in the ternary plot: {filtered_phases_tern}")
@@ -28,11 +28,10 @@ dof_tern = len(tern_comp) - len(filtered_phases_tern) + 2
 print(f"Degrees of freedom for the ternary plot: {dof_tern}")
 
 fig, ax = plt.subplots(figsize=(10, 7), dpi=100)
-wks = Workspace(dbf, tern_comp, filtered_phases_tern, conds_tern, verbose=True)
-results = wks.get(v.T, v.X('C'))
+wks = Workspace(dbf, tern_comp, phase_keys, conds_tern, verbose=True)
 
 ax.set_title(f'Gibbs energy vs temperature in Fe-C')
 ax.set_xlabel('Temperature (K)')
-ax.set_ylabel('Gibbs Energy (J/mol)')
+ax.set_ylabel('')
 ax.legend(loc='best')
 plt.show()

3f1b409c6ac97e7aed023c7cbc33ba5f615ffb85
Soki Sem <seminaia401@gmail.com>
2026-06-10 03:19:32 +0000

    Jun 9, 2026, 11:19 PM

--- CALPHAD/pycal_teset.py
+++ CALPHAD/pycal_teset.py
@@ -15,10 +15,10 @@ tern_comp = ['FE', 'C', 'VA']
 phase_keys = list(dbf.phases.keys())  # Get all phase keys from the database
 filtered_phases_tern = filter_phases(dbf, tern_comp, phase_keys)  # Filter phases based on components
 conds_tern = {
-    v.T: (1000, 2000,100),  # vary from 1000 to 2000 step 100
+    v.T: (1600, 2000,10),  # vary from 1600 to 2000 step 10
     v.P: 101325,
     v.N: 1,
-    v.X('C'): (0, 0.08, 0.01),  # vary from 0 to 0.08 step 0.01
+    v.X('C'): (0, 0.08, 0.001),  # vary from 0 to 0.08 step 0.001
 }    
 
 print(f"Phases considered in the ternary plot: {filtered_phases_tern}")
@@ -27,5 +27,7 @@ print(f"Number of phases in the ternary plot: {len(filtered_phases_tern)}")
 
 dof_tern = len(tern_comp) - len(filtered_phases_tern) + 2
 print(f"Degrees of freedom for the ternary plot: {dof_tern}")
-
-binplot(dbf, tern_comp,phase_keys, conds_tern)
+fig = plt.figure(figsize=(8, 6))
+ax = fig.gca()
+binplot(dbf, tern_comp,phase_keys, conds_tern,plot_kwargs={'ax':ax})
+plt.show()

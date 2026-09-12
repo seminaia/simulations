0c7a7144668ce3f293541232e7603597b82c7138
seminaia <seminaia401@gmail.com>
2026-09-12 21:47:29 +0000

    Sep 12, 2026, 5:47 PM

--- MTE509_HW2.py
+++ MTE509_HW2.py
@@ -164,8 +164,8 @@ p1 = ((E1 / c)**2 - m_e**2)**0.5
 p2 = ((E2 / c)**2 - m_e**2)**0.5
 
 # Calculate radius (removing the extra factor of 2 so it is radius, not diameter)
-r1 = p1 / 2 / (e * B) * 1e3 
-r2 = p2 / 2 / (e * B) * 1e3
+r1 = p1 / (e * B) * 1e3 
+r2 = p2 / (e * B) * 1e3
 
 
 print(f"r1: {r1:.4f} mm, r2: {r2:.4f} mm")

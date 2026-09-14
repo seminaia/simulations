f9db30d4adaf1d98d049273a68106e4c7b37aca5
Soki Sem <seminaia401@gmail.com>
2026-09-14 18:04:05 +0000

    Sep 14, 2026, 2:04 PM

--- CALPHAD/pycal_test.py
+++ CALPHAD/pycal_test.py
@@ -8,8 +8,10 @@ import pandas as pd
 from pycalphad.property_framework.metaproperties import IsolatedPhase
 # Load database
 
-f = "mf-steel-3g.tdb"
-with open(f, 'r',encoding='latin-1') as file:
+f1 = "Fe-C.TDB"
+f2 = "mmc1.TDB"
+f3 = "mf-steel-3g.tdb"
+with open(f1, 'r',encoding='latin-1') as file:
     content = file.read()
 dbf = Database(content)
 tern_comp = ['FE', 'C', 'VA']
@@ -19,10 +21,10 @@ print("All phase keys from the database:", phase_keys)
 filtered_phases_tern = filter_phases(dbf, tern_comp, phase_keys)  # Filter phases based on components
 print("Filtered phases for the ternary plot:", list(filtered_phases_tern))
 conds_tern = {
-    v.T: (800, 1800,100),  # vary from 1600 to 2000 step 10
+    v.T: (800, 1800,10),  # vary from 1600 to 2000 step 10
     v.P: 101325,
     v.N: 1,
-    v.X('C'): (0, 0.8, 0.1),  # vary from 0 to 0.08 step 0.001
+    v.X('C'): (0, 1, 0.1),  # vary from 0 to 0.08 step 0.001
 }    
 print(f"Phases considered in the ternary plot: {filtered_phases_tern}")
 print(f"Components considered in the ternary plot: {tern_comp}")
@@ -30,10 +32,11 @@ print(f"Number of phases in the ternary plot: {len(filtered_phases_tern)}")
 
 dof_tern = len(tern_comp) - len(filtered_phases_tern) + 2
 print(f"Degrees of freedom for the ternary plot: {dof_tern}")
-fig = plt.figure(figsize=(8, 6))
-ax = fig.gca()
 bin_strat= BinaryStrategy(dbf, tern_comp, phase_keys, conds_tern)
-bin_strat.generate_automatic_starting_points()
 bin_strat.do_map()
-plot = plot_binary(bin_strat,x=v.X('C'),y=v.T)
+ax = plot_binary(bin_strat,x=v.X('C'),y=v.T)
+fig = ax.figure
+ax.set_title("Binary Phase Diagram for FE-C System")
+ax.set_xlabel("C Mole Fraction")
+ax.set_ylabel("Temperature (K)")
 plt.show()

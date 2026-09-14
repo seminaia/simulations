cea5c839362d4e510e5dc1c9fbda4b3b4c7c4900
Soki Sem <seminaia401@gmail.com>
2026-09-14 16:38:05 +0000

    Sep 14, 2026, 12:38 PM

--- CALPHAD/pycal_test.py
+++ CALPHAD/pycal_test.py
@@ -8,7 +8,7 @@ import pandas as pd
 from pycalphad.property_framework.metaproperties import IsolatedPhase
 # Load database
 
-f = "steel_database_fix.tdb"
+f = "mmc1.TDB"
 with open(f, 'r',encoding='latin-1') as file:
     content = file.read()
 dbf = Database(content)
